491ea800d777c647604fedcb1ff00300ead20de3
Gwen <gwenmillerpathways@gmail.com>
2018-02-15 18:16:49 -0500

    Combined Elevator and Break
    Combined two java classes

--- src/org/usfirst/frc/team181/robot/Elevator.java
+++ src/org/usfirst/frc/team181/robot/Elevator.java
@@ -1,12 +1,15 @@
 //Created by Gwen 2018
 package org.usfirst.frc.team181.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 
 public class Elevator {
 	//Defines variables
 	static Spark m_elevator = new Spark(2);
+	
+	static DoubleSolenoid BrakeSol = new DoubleSolenoid(0,4,5);
 
 	public static Joystick opstick;
 	
@@ -18,6 +21,27 @@ public class Elevator {
 		m_elevator.set(y);	//sets the input for "elemethod" to the "y" for the Spark motor-controller
 	}
 	
+	public static void brakeOn() {
+		BrakeSol.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	public static void brakeOff() {
+		BrakeSol.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	public static void Brake() {
+		if(opstick.getRawButton(1) == true && opstick.getRawButton(4) == false) {
+			brakeOn();
+			System.out.println("Brake Engaged!");
+		}
+		if(opstick.getRawButton(1) == false && opstick.getRawButton(4) == true) {
+			brakeOff();
+			System.out.println("Brake Disengaged!");
+		}
+		if(opstick.getRawButton(1) == false && opstick.getRawButton(4) == true) {
+			
+		}
+	}
 
 }
 
